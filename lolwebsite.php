3614307af95b938ee91cdfa3e82507bbc9f07804
Richard Kasendu <kasendu@gmail.com>
2017-11-17 14:21:52 +0100

    Unfinished base website

--- lolwebsite.php
+++ lolwebsite.php
@@ -14,16 +14,22 @@
     <?php
       $curl = curl_init();
       $API = "RGAPI-11c9915b-0d34-405a-867b-2c5142056a86";
-      $link = "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RichyFTW?api_key=RGAPI-11c9915b-0d34-405a-867b-2c5142056a86";
+      $link = "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RichyFTW";
 
 
       curl_setopt_array($curl, array(
         CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $link
+        CURLOPT_URL => $link,
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => array(
+          'api_key' => "RGAPI-11c9915b-0d34-405a-867b-2c5142056a86"
+        )
       ));
 
       $result = curl_exec($curl);
       echo $result;
+
+      curl_close($curl);
 
     ?>
 
