5fec1e430bead77d9948a1437c201b7e3a4c08aa
Richard <kasendu@gmail.com>
2017-11-20 01:10:46 +0100

    Made MySqlI database and grounds for satic data. Optimized starting page mostly done with that. Added a summoner profile pic

---
+++ assets/php/extended.php
@@ -0,0 +1,130 @@
+<?php
+class STATIK {
+    const API = "RGAPI-9968c039-8800-4fb8-ae3f-de4fb286de81";
+
+    const SUMMONER_NAME = "/lol/summoner/v3/summoners/by-name/";
+    const VERSIONS = "/lol/static-data/v3/versions/";
+    const RANKS = "/lol/league/v3/positions/by-summoner/";
+}
+    
+
+$userData = array(
+    "summonerName" => $_GET["summonerName"]
+    /*
+    First request to get these variables
+    "id" 
+    "accountId" 
+    "name" 
+    "profileIconId" 
+    "revisionDate" 
+    "summonerLevel"
+    
+    **/
+
+);
+
+$curl = curl_init();
+
+
+$parameters = http_build_query([
+        "?".'api_key' => STATIK::API
+    
+        ]);
+$parameters = urldecode($parameters);
+$prefix = "https://euw1.api.riotgames.com";
+$options = array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $prefix.STATIK::SUMMONER_NAME.$userData["summonerName"].$parameters,
+);
+
+curl_setopt_array($curl, $options); 
+$result = json_decode(curl_exec($curl), true); // Converting to array. Second arguemnt true makes it so instead convertying to object it converts to array.
+forEach($result as $key => $value) {
+    $userData[$key] = $value;
+}
+curl_close($curl); 
+
+function getLatestVersion() { // Gets the latest game
+     $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $GLOBALS['prefix'].STATIK::VERSIONS.$GLOBALS["parameters"]
+    ));
+    
+    $result = json_decode(curl_exec($curl), true);
+    //mysqlConnection($result);
+    
+    curl_close($curl);
+    if(array_key_exists("status", $result)) {
+        $connection = mysqli_connect("localhost", "root", "", "barcode");
+        $sql = "SELECT MAX(versions) FROM static_data";
+        $result = mysqli_fetch_array(mysqli_query($connection, $sql));
+
+        mysqli_close($connection);
+        return $result[0];
+    } else {
+        mysqlConnection($result);
+        return $result[0];
+    }
+    
+}
+
+function getRanks() {
+    $curl = curl_init();
+
+    $userData = $GLOBALS["userData"];
+
+
+    $options = array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $GLOBALS["prefix"].STATIK::RANKS.$userData['id'].$GLOBALS["parameters"],
+    );
+
+    curl_setopt_array($curl, $options);
+    $result = json_decode(curl_exec($curl), true);
+
+    
+    forEach($result as $key => $value) {
+        unset($result[$key]['leagueId']);
+        unset($result[$key]['veteran']);
+        unset($result[$key]['freshBlood']);
+        unset($result[$key]['hotStreak']);       
+    }
+
+
+    curl_close($curl);
+    return $result;
+}
+
+function mysqlConnection($data) {
+    $servername = "localhost";
+    $username = "root";
+
+    $conn = mysqli_connect($servername, $username, "", "barcode");
+    if(!$conn) {
+        echo "could not connect to server" .mysql_error();
+    }
+    
+    for($n = 0; $n <= 5; $n++) {
+        $check = mysqli_query($conn, "SELECT * FROM static_data WHERE versions = '".$data[$n]."'");
+        if(mysqli_num_rows($check) > 0) {
+            return;
+        } else {
+            $sql = "INSERT IGNORE INTO static_data (versions) VALUES ('".$data[$n]."')";
+            $retval = mysqli_query($conn, $sql);
+            if(!$retval) {
+                echo "Error: ".mysqli_error();
+            } 
+        }
+        
+    }
+   
+
+    mysqli_close($conn);
+    
+}
+
+
+
+?>
--- lolwebsite.php
+++ lolwebsite.php
@@ -1,37 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/lolwebsite.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body>
-    You typed in <?php echo $_GET["summonerName"]; ?>
-    <br>
-    Region is <?php echo $_GET["regionList"]; ?>
-
     <?php
-      $curl = curl_init();
-      $API = "RGAPI-11c9915b-0d34-405a-867b-2c5142056a86";
-      $link = "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RichyFTW";
+      include('assets/php/extended.php'); // Including php in my html 
+      ?>
+        <div id="wrapper">
+            <div id="summoner-div" class="child">
+                <img src="http://ddragon.leagueoflegends.com/cdn/<?php echo getLatestVersion() ?>/img/profileicon/<?php echo $userData['profileIconId'] ?>.png" id="profileIcon">
+                
+                <?php 
+                    $ranks = getRanks();
+                    forEach($ranks as $rank) {
+                       
+                        echo "<div class='rank-div'>";
+                        echo "League: ".$rank['leagueName'];
+                        echo "<br>";
+                        echo "Tier: ".$rank['tier'].$rank['rank'];
+                        echo "<br>";
+                        echo "Queue: ".$rank['queueType'];
+                        echo "<br>";
+                        echo "Wins: ".$rank['wins'];
+                        echo "<br>";
+                        echo "Losses: ".$rank['losses'];
+                        echo "</div>";
+                    }
+                
+                ?>
+                
+            </div>
+            <div id="split" class="child">
+                <div id="top10-div" class="babies"></div>
+                <div id="mostactive-div" class="babies"></div>
+            </div>
 
+            <div id="decypher-div" class="child"></div>
+        </div>
 
-      curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $link,
-        CURLOPT_POST => 1,
-        CURLOPT_POSTFIELDS => array(
-          'api_key' => "RGAPI-11c9915b-0d34-405a-867b-2c5142056a86"
-        )
-      ));
-
-      $result = curl_exec($curl);
-      echo $result;
-
-      curl_close($curl);
-
-    ?>
 
 </body>
+
 </html>
